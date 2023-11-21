0165466d8c5c2a4fdd9574523daaab1d0f6dc9ad
galenomoon <galeno.dev@gmail.com>
2023-11-21 13:24:13 -0300

    Refactor getUserRepositories function

--- src/modules/repositories/User/getUserRepositories/getUserRepositories.js
+++ src/modules/repositories/User/getUserRepositories/getUserRepositories.js
@@ -6,10 +6,7 @@ const {
 const getUserRepositories = async ({
     user_id
 } = {}) => {
-
-    const {
-        response
-    } = await client('users').where({ id: user_id })
+    const response = await client('users').where({ id: user_id })
 
     const has_response = Array.isArray(response) && response.length > 0;
 
@@ -19,11 +16,9 @@ const getUserRepositories = async ({
         }
     }
 
-    // return {
-    //     users: response
-    // }
-    return undefined;
-
+    return {
+        users: response
+    }
 }
 
 module.exports = {
